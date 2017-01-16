246755e0d958740bd22b86e3daf92882d6796560
Love Wikberg <love.wick93@gmail.com>
2017-01-16 10:08:32 +0100

    Till pascal

---
+++ NumberRoman/RomanNumeralConverter/RomanNumeralConverter.cs
@@ -0,0 +1,129 @@
+﻿#region Usings
+using System;
+using System.Collections.Generic;
+using System.Text;
+#endregion
+// DO NOT CHANGE
+namespace NumberSystemConverter
+{
+    #region Container for allowing management of RomanNumeralsType enumerals
+    struct RomanNumeralPair
+    {
+        public int NumericValue { get; set; }
+        public string RomanNumeralRepresentation { get; set; }
+    }
+    #endregion
+    #region Roman Numerals - Seven symbols(enum RomanNumeralsType) with respective value.
+    enum RomanNumeralsType
+    {
+        M = 1000,
+        D = 500,
+        C = 100,
+        L = 50,
+        X = 10,
+        IX = 9,
+        V = 5,
+        IV = 4,
+        I = 1
+    }
+    #endregion
+    public class RomanNumeralConverter
+    {
+        // Readonly - The variable assigned with the readonly operator can only be changed inside the declaration or in the constructor
+        private List<RomanNumeralPair> romanNumeralList;
+
+        public RomanNumeralConverter()
+        {
+            #region Initializing the list with romanNumerals
+            romanNumeralList = new List<RomanNumeralPair>()
+            {
+                new RomanNumeralPair() {
+                    // 1000
+                    NumericValue = (int)RomanNumeralsType.M,
+                    RomanNumeralRepresentation = RomanNumeralsType.M.ToString()
+                },
+                new RomanNumeralPair() {
+                    // 500
+                    NumericValue = (int)RomanNumeralsType.D,
+                    RomanNumeralRepresentation = RomanNumeralsType.D.ToString()
+                },
+                new RomanNumeralPair() {
+                    // 100
+                    NumericValue = (int)RomanNumeralsType.C,
+                    RomanNumeralRepresentation = RomanNumeralsType.C.ToString()
+                },
+                new RomanNumeralPair() {
+                    // 50
+                    NumericValue = (int)RomanNumeralsType.L,
+                    RomanNumeralRepresentation = RomanNumeralsType.L.ToString()
+                },
+                new RomanNumeralPair() {
+                    // 10
+                    NumericValue = (int)RomanNumeralsType.X,
+                    RomanNumeralRepresentation = RomanNumeralsType.X.ToString()
+                },
+                 new RomanNumeralPair() {
+                    // 9
+                    NumericValue = (int)RomanNumeralsType.IX,
+                    RomanNumeralRepresentation = RomanNumeralsType.IX.ToString()
+                },
+                new RomanNumeralPair() {
+                    // 5
+                    NumericValue = (int)RomanNumeralsType.V,
+                    RomanNumeralRepresentation = RomanNumeralsType.V.ToString()
+                },
+                 new RomanNumeralPair() {
+                    // 4
+                    NumericValue = (int)RomanNumeralsType.IV,
+                    RomanNumeralRepresentation = RomanNumeralsType.IV.ToString()
+                },
+                new RomanNumeralPair() {
+                    // 1
+                    NumericValue = (int)RomanNumeralsType.I,
+                    RomanNumeralRepresentation = RomanNumeralsType.I.ToString()
+                }
+            };
+            #endregion 
+        }
+
+        //public int ConvertToInteger(string input)
+        //{
+        //    char[] array = input.ToCharArray();
+        //    int result = 0;
+        //    for (int i = 0; i < array.Length; i++)
+        //    {
+        //        if(array[i] == 'I')
+        //        {
+        //            result += 1;
+        //        }
+        //        if (array[i] == 'V')
+        //        {
+        //            result += 5;
+        //        }
+        //    }
+        //}
+
+        public string ConvertToRomanNumeral(int number)
+        {
+            StringBuilder builder = new StringBuilder();
+            if (number > 3001)
+                throw new Exception("Too high value");
+            else if (number < 1)
+                throw new Exception("Too low value");
+            else
+            {
+                // Iterate through the list, starting with the highest value
+                foreach (var currentPair in romanNumeralList)
+                {
+                    while (number >= currentPair.NumericValue)
+                    {
+                        // ...number is greater than or equal to the current value so store the roman numeral and decrement it's value
+                        builder.Append(currentPair.RomanNumeralRepresentation);
+                        number -= currentPair.NumericValue;
+                    }
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
---
+++ NumberRoman/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumberSystemConverter;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ThreeShouldBeIII()
+        {
+            RomanNumeralConverter test = new RomanNumeralConverter();
+            int input = 3;
+            string expectedOutput = "III";
+            Assert.AreEqual(expectedOutput, test.ConvertToRomanNumeral(input));
+        }
+
+        [TestMethod]
+        public void HoundredShouldBeC()
+        {
+            RomanNumeralConverter test = new RomanNumeralConverter();
+            int input = 100;
+            string expectedOutput = "C";
+            Assert.AreEqual(expectedOutput, test.ConvertToRomanNumeral(input));
+        }
+
+        [TestMethod]
+        public void OneThousandThirtySixShouldBeMXXXVI()
+        {
+            RomanNumeralConverter test = new RomanNumeralConverter();
+            int input = 1036;
+            string expectedOutput = "MXXXVI";
+            Assert.AreEqual(expectedOutput, test.ConvertToRomanNumeral(input));
+        }
+
+        [TestMethod]
+        public void SixHoundredSevenShouldBeDCVII()
+        {
+            RomanNumeralConverter test = new RomanNumeralConverter();
+            int input = 607;
+            string expectedOutput = "DCVII";
+            Assert.AreEqual(expectedOutput, test.ConvertToRomanNumeral(input));
+        }
+
+        [TestMethod]
+        public void TwoThousandFiftyShouldBeMML()
+        {
+            RomanNumeralConverter test = new RomanNumeralConverter();
+            int input = 2050;
+            string expectedOutput = "MML";
+            Assert.AreEqual(expectedOutput, test.ConvertToRomanNumeral(input));
+        }
+
+        [TestMethod]
+        public void OneShouldBeI()
+        {
+            RomanNumeralConverter test = new RomanNumeralConverter();
+            int input = 1;
+            string expectedOutput = "I";
+            Assert.AreEqual(expectedOutput, test.ConvertToRomanNumeral(input));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void ZeroShouldBeException()
+        {
+            RomanNumeralConverter test = new RomanNumeralConverter();
+            int input = 0;
+            test.ConvertToRomanNumeral(input);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void BadNumberShouldBeException()
+        {
+            RomanNumeralConverter test = new RomanNumeralConverter();
+            int input = -00000000;
+            test.ConvertToRomanNumeral(input);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void NegativeIntShouldBeException()
+        {
+            RomanNumeralConverter test = new RomanNumeralConverter();
+            int input = -3;
+            test.ConvertToRomanNumeral(input);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void IfInputIsHigherThan_3001_Exception()
+        {
+            RomanNumeralConverter test = new RomanNumeralConverter();
+            int input = 3005;
+            test.ConvertToRomanNumeral(input);
+        }
+
+
+    }
+}
